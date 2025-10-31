79cbc1fd2f91b254ea0299660ae612708c5bddb4
Nevena Chupetlovska <nchupetl@smail.uni-koeln.de>
2025-10-31 18:51:12 +0100

    Initial commit

---
+++ src/h1/H1_main.java
@@ -0,0 +1,20 @@
+package h1;
+
+public class H1_main {
+
+	public static void main(String[] args) {
+		        // z deklarieren
+		        int z = 15;
+
+		        // Aufgabe: ist z>10 ? Dann z um 10 verkleinern, sonst nichts ändern
+		        if (z > 10) {
+		            z = z - 10;
+		        }
+
+		        System.out.println("z = " + z);
+		    		}
+
+
+	}
+
+
