f5e874aeb0350d236396fb3ed41637ceaa77a40d
Nevena Chupetlovska <nchupetl@smail.uni-koeln.de>
2025-10-31 19:36:47 +0100

    HA2

---
+++ src/h2/H2_main.java
@@ -0,0 +1,38 @@
+package h2;
+
+public class H2_main {
+
+	public static void main(String[] args) {
+		        int i = 64;
+		        int j = 9;
+		        int k = 28;
+
+		        int min;
+		        int max;
+
+		        // Minimum finden
+		        if (i < j && i < k) {
+		            min = i;
+		        } else if (j < i && j < k) {
+		            min = j;
+		        } else {
+		            min = k;
+		        }
+
+		        // Maximum finden
+		        if (i > j && i > k) {
+		            max = i;
+		        } else if (j > i && j > k) {
+		            max = j;
+		        } else {
+		            max = k;
+		        }
+
+		        System.out.println("i = " + i + ", j = " + j + ", k = " + k);
+		        System.out.println("Minimum = " + min);
+		        System.out.println("Maximum = " + max);
+		    }
+
+
+	}
+
